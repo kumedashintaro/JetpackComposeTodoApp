04baeb8cdae99884c7a31c11963237a1e1769399
kumedashintaro <pii.k2.bys@gmail.com>
2023-07-25 11:01:39 +0900

    add FAB

--- app/src/main/java/com/example/jetpackcomposetodoapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetodoapp/MainActivity.kt
@@ -1,11 +1,21 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.jetpackcomposetodoapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.jetpackcomposetodoapp.ui.theme.JetpackComposeTodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,9 +31,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainContent() {
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
+        }
+    }) {
+
     }
 }
